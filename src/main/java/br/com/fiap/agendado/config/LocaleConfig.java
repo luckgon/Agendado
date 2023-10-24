a4a0569a6f93cb3a5d32a610bafc24d552293472
luckgon <rm88349@fiap.com.br>
2023-10-24 15:57:17 -0300

    up

--- src/main/java/br/com/fiap/agendado/config/LocaleConfig.java
+++ src/main/java/br/com/fiap/agendado/config/LocaleConfig.java
@@ -5,18 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-
 @Configuration
 public class LocaleConfig {
 
     @Bean
-    public MessageSource messageSource() {
-
+    public MessageSource messageSource(){
         var messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("lang/messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-
     
 }
